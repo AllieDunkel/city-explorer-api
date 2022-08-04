9bb3f7800e3b7d10a9c8a542f09dc4acd5d23de5
Allie Dunkel <alliedunkel@outlook.com>
2022-08-03 21:19:16 -0500

    added some files and started the API

---
+++ server.js
@@ -0,0 +1,20 @@
+'use strict'
+console.log('server is connected!!!')
+
+//Require
+// ===packages
+const express= require('express');
+
+const weatherData = require("./data/weather.json");
+
+
+//Use
+
+
+//Classes
+
+//Errors
+
+//Routes
+
+//Listen
